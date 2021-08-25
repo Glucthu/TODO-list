780f15987cf630c9f44d54f622c8e2de806f596d
glucthu <glucthu@protonmail.com>
2021-08-25 16:24:04 -0300

    added format

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 function addNewItem()
 {
+    let format = /[a-zA-Z]/g;
     let table = document.getElementById("items");
     let newItem = document.getElementById("new-item").value;
 
-    if(newItem != "")
+    if(newItem != "" && format.test(newItem))
     {
         let row = table.insertRow(0);
         let cell = row.insertCell(0);
