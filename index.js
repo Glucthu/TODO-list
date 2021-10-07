d42a91863404a19b739895c8fa5e3ec3a5bf7133
Glucthu <glucthu@protonmail.com>
2021-10-07 17:00:43 -0300

    added bootstrap

--- index.js
+++ index.js
@@ -3,11 +3,22 @@ function addNewItem()
     let format = /[a-zA-Z]/g;
     let table = document.getElementById("items");
     let newItem = document.getElementById("new-item").value;
+		let exist = false;
+		
+		for (var i = 0, row; row = table.rows[i]; i++) {
+   		for (var j = 0, col; col = row.cells[j]; j++) {
+				if(row.cells[j].id == 'i-' + "'" + newItem + "'") {
+					exist = true;
+				}
+   		}  
+		}
 
-    if(newItem != "" && format.test(newItem))
+
+    if(newItem != "" && format.test(newItem) && !exist)
     {
         let row = table.insertRow(0);
         let cell = row.insertCell(0);
-        cell.innerHTML = newItem + '<input type="checkbox" name="'+ newItem +'"></input>';
+			cell.innerHTML = newItem + '<div class="item-inputs"><input type="checkbox" name="'+ newItem +'"></input>' + '<button type="button" class="btn btn-danger rm-item" onclick="delItem(\'i-\'' + newItem + '\'\')"><i class="fas fa-trash"></i></button></div>';
+				cell.id = 'i-' + "'" + newItem + "'";
     }
 }
