72bd7173a1c6890ae85fd6c0fab555fcab8d57b9
Glucthu <glucthu@protonmail.com>
2021-10-12 13:49:12 -0300

    remove items

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-function addNewItem()
-{
+function addNewItem() {
     let format = /[a-zA-Z]/g;
     let table = document.getElementById("items");
     let newItem = document.getElementById("new-item").value;
@@ -7,7 +6,7 @@ function addNewItem()
 		
 		for (var i = 0, row; row = table.rows[i]; i++) {
    		for (var j = 0, col; col = row.cells[j]; j++) {
-				if(row.cells[j].id == 'i-' + "'" + newItem + "'") {
+				if(row.cells[j].id == 'i-' + newItem ) {
 					exist = true;
 				}
    		}  
@@ -18,7 +17,11 @@ function addNewItem()
     {
         let row = table.insertRow(0);
         let cell = row.insertCell(0);
-			cell.innerHTML = newItem + '<div class="item-inputs"><input type="checkbox" name="'+ newItem +'"></input>' + '<button type="button" class="btn btn-danger rm-item" onclick="delItem(\'i-\'' + newItem + '\'\')"><i class="fas fa-trash"></i></button></div>';
-				cell.id = 'i-' + "'" + newItem + "'";
+			cell.innerHTML = newItem + '<div class="item-inputs"><input type="checkbox" name="'+ newItem +'"></input>' + '<button type="button" class="btn btn-danger rm-item" onclick="delItem(\'i-' + newItem + '\')"><i class="fas fa-trash"></i></button></div>';
+				cell.id = 'i-' + newItem;
     }
+}
+
+function delItem(id) {
+	document.getElementById(id).remove();
 }
